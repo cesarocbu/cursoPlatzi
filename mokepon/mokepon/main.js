189491b1e9787e0b59441b398014eb7bf7859f63
cesarocbu <cesar_8ab@hotmail.com>
2022-09-14 23:49:19 -0400

    Funciones de mascotas

--- mokepon/mokepon/main.js
+++ mokepon/mokepon/main.js
@@ -7,18 +7,49 @@ function seleccionarMascotaJugador(){
     let inputHipo = document.getElementById('Hipodoge');
     let inputCapi = document.getElementById('Capipepo');
     let inputRati = document.getElementById('Ratigueya');
-
+    let inputLango = document.getElementById('Langostelvis');
+    let inputTuca = document.getElementById('Tucapalma');
+    let inputpydos = document.getElementById('Pydos');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
 
     if(inputHipo.checked){
-        alert("Hipodoge es tu mascota!")
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if(inputCapi.checked){
-        alert("Capipepo es tu mascota!")
+        spanMascotaJugador.innerHTML = 'Capipepo'
     } else if(inputRati.checked){
-        alert("Ratigueya es tu mascota!")
-    } else{
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } // else if(inputLango.checked){
+    //     spanMascotaJugador.innerHTML = 'Langostelvis'
+    // } else if(inputLango.checked){
+    //     spanMascotaJugador.innerHTML = 'Tucapalma'
+    // } else if(inputpydos.checked){
+    //     spanMascotaJugador.innerHTML = 'Pydos'
+    // }
+     else{
         alert("Debes seleccionar una mascota")
     }
 
+    seleccionarMascotaEnemigo();
+
+}
+
+function seleccionarMascotaEnemigo(){
+
+    let ataqueAleatorio = aleatorio(1,3);
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+    if(ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if(ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else if(ataqueAleatorio == 3){
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', iniciarJuego)
